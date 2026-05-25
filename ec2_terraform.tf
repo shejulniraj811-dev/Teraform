0374d6014bd84f476df4803e47afee94943b99ad
Niraj Shejul <shejulniraj811@gmail.com>
2026-05-25 18:32:49 +0530

    final terraform file

--- ec2_terraform.tf
+++ ec2_terraform.tf
@@ -5,9 +5,9 @@ resource "aws_instance" "myEc2instance" {
   ami           = "ami-091138d0f0d41ff90"
   instance_type = "t3.micro"
   key_name = "aws_key.pem"
-  vpc_security_group = "sg-0981f1bdf4af6c11d"
+  vpc_security_group_ids = ["sg-0981f1bdf4af6c11d"] 
 
-  tags = {
+ tags = {
     Name = "Niraj"
     name = "Niraj"
     env = "dev"
